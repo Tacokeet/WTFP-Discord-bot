ac9f5804760650627dd5aa36c2335a1f9c3b05ec
Sander Ruchti <sander@topay.nl>
2021-11-18 19:36:51 +0100

    Added Birthday functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WTFP-Discord-bot
+
+#### Bot should include a .env file with the following:
+- DISCORD_TOKEN=
+- SOUNDFILE_PATH=path/to/soundbites/
+- BIRTHDAY_MESSAGES_PATH=path/to/birthday_messages
+- STREEPJES_DB=streepjesDB.json
+- CALENDAR_DB=calendarDB.json
--- bot.py
+++ bot.py
@@ -7,8 +7,10 @@ import youtube_dl
 import validators
 import aiohttp_jinja2
 import jinja2
+import datetime
+from datetime import date
 from aiohttp import web
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from tinydb import TinyDB, Query
 from tinydb.operations import increment
@@ -345,6 +347,7 @@ class Streepje(commands.Cog):
     db = TinyDB(path_to_database)
     User = Query()
     streepjes_messages = {}
+    available_text_channels = []
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -361,7 +364,7 @@ class Streepje(commands.Cog):
                                            self.User.name == self.streepjes_messages[reaction.message.id][0])
                         await reaction.message.channel.send(
                             "{} now has {} streepje(s)".format(
-                                bot.get_user(self.streepjes_messages[reaction.message.id][0]).name, self.db.search(
+                                self.bot.get_user(self.streepjes_messages[reaction.message.id][0]).name, self.db.search(
                                     self.User.name == self.streepjes_messages[reaction.message.id][0])[0]['streepjes']))
 
     @commands.command(name="streepjeslist")
@@ -542,6 +545,132 @@ class Website(commands.Cog):
         asyncio.ensure_future(self.site.stop())
 
 
+class Calender(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bd_alert.start()
+        if not os.path.exists(self.path_to_database):
+            open(self.path_to_database, 'w+')
+
+    load_dotenv()
+    path_to_database = str(os.getenv('CALENDAR_DB'))
+    path_to_birthday_messages = str(os.getenv('BIRTHDAY_MESSAGES_PATH'))
+    db = TinyDB(path_to_database)
+    User = Query()
+    available_text_channels = []
+    birthday_role = discord.role
+    player_queue = []
+    vc = discord.voice_client
+
+    def play_next_bd_message(self):
+        if len(self.player_queue) >= 1:
+            self.player_queue.pop(0)
+            try:
+                self.vc.play(self.player_queue[0], after=lambda e: self.play_next_bd_message())
+            except IndexError:
+                return
+        else:
+            return
+
+    async def play_bd_message(self, filename):
+        if self.vc.is_playing():
+            self.player_queue.append(discord.FFmpegPCMAudio(filename))
+            return
+
+        player = discord.FFmpegPCMAudio(filename)
+        self.player_queue.append(player)
+        self.vc.play(player, after=lambda e: self.play_next_bd_message())
+
+    @commands.command(name="mybd")
+    async def my_bd(self, ctx, *, birthday):
+        """Add birthday D-M-Y (1-11-1996) to yourself"""
+        if '-' in birthday:
+            birthday_array = birthday.split('-')
+            day = birthday_array[0]
+            month = birthday_array[1]
+            year = birthday_array[2]
+        elif '/' in birthday:
+            birthday_array = birthday.split('/')
+            day = birthday_array[0]
+            month = birthday_array[1]
+            year = birthday_array[2]
+        else:
+            birthday_array = birthday.split()
+            day = birthday_array[0]
+            month = birthday_array[1]
+            year = birthday_array[2]
+
+        try:
+            date(int(year), int(month), int(day))
+        except Exception as e:
+            await ctx.send(e)
+
+        if not self.db.search(self.User.name == ctx.author.id):
+            self.db.insert(
+                {'name': ctx.author.id, 'day': int(day), 'month': int(month), 'year': int(year), 'alert': False})
+        else:
+            self.db.update({'day': int(day), 'month': int(month), 'year': int(year)}, self.User.name == ctx.author.id)
+        await ctx.send("Noted! " + ctx.author.name + " your birthday is on " + day + "-" + month)
+
+    @commands.command(name="notifybd")
+    async def notify_bd(self, ctx):
+        """Add yourself to the notify list to get notified when someone's birthday is within 2 weeks"""
+        await ctx.author.add_roles(self.birthday_role)
+        await ctx.send("Added " + ctx.author.name + " to the notify list for birthdays!")
+
+    @tasks.loop(hours=1)  # every hour
+    async def bd_alert(self):
+        now = datetime.datetime.now()
+        two_weeks_from_now = now + datetime.timedelta(days=14)
+        if now.hour == 22:  # should be 8
+            for user in self.db:
+                if user['day'] is now.day and user['month'] is now.month:
+                    await self.available_text_channels[0].send(
+                        "🎉 Happy Birthday " + self.bot.get_user(user['name']).mention + " You turned " + str(
+                            now.year - user['year']) + " years old! 🎉")
+                if user['day'] is two_weeks_from_now.day and user['month'] is two_weeks_from_now.month:
+                    self.db.update({'alert': False}, self.User.name == user['name'])
+                    await self.available_text_channels[0].send(self.birthday_role.mention + ' ' + self.bot.get_user(
+                        user['name']).name + " his birthday will be in two weeks don't forget to buy some presents! 🎁")
+        for guild in self.bot.guilds:
+            for user in self.db:
+                member = await guild.fetch_member(user['name'])
+                if member.voice is not None and user['day'] is now.day and user['month'] is now.month and not user[
+                    'alert']:
+                    self.vc = await member.voice.channel.connect()
+                    await self.play_bd_message(self.path_to_birthday_messages + 'hey.mp3')
+                    await self.play_bd_message(self.path_to_birthday_messages + str(
+                        user['name']) + '.mp3')
+                    await self.play_bd_message(
+                        self.path_to_birthday_messages + 'gefeliciteerd.mp3')
+                    await self.play_bd_message(
+                        self.path_to_birthday_messages + 'met.mp3')
+                    await self.play_bd_message(
+                        self.path_to_birthday_messages + 'je.mp3')
+                    await self.play_bd_message(
+                        self.path_to_birthday_messages + 'verjaardag.mp3')
+                    self.db.update({'alert': True}, self.User.name == user['name'])
+
+    @bd_alert.before_loop
+    async def before_bd_alert(self):
+        await self.bot.wait_until_ready()
+        for server in self.bot.guilds:
+            add_birthday_role = True
+
+            for role in await server.fetch_roles():
+                if role.name == 'Birthday':
+                    self.birthday_role = role
+                    add_birthday_role = False
+
+            if add_birthday_role:
+                self.birthday_role = await server.create_role(name="Birthday", colour=discord.Color.gold())
+
+            for channel in server.channels:
+                if str(channel.type) == 'text':
+                    if channel.permissions_for(server.me).send_messages:
+                        self.available_text_channels.append(channel)
+
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -589,7 +718,7 @@ async def leave(ctx):
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    """Disconnects from voicechannel after 60s of inactivity."""
+    """Disconnects from voicechannel after 230s of inactivity."""
     if not member.id == bot.user.id:
         return
 
@@ -601,7 +730,7 @@ async def on_voice_state_update(member, before, after):
             time = time + 1
             if voice.is_playing() and not voice.is_paused():
                 time = 0
-            if time == 60:
+            if time == 230:
                 await voice.disconnect()
             if not voice.is_connected():
                 break
@@ -612,6 +741,7 @@ bot.add_cog(Music(bot))
 bot.add_cog(Streepje(bot))
 bot.add_cog(Jeopardy(bot))
 bot.add_cog(Together(bot))
+bot.add_cog(Calender(bot))
 
 website = Website(bot, messages, errors)
 bot.add_cog(website)
